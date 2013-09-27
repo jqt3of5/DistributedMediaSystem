334c4f7046f973cc7167e71b1e988cd82cc4d27c
jqt3of5 <jqt3of5@gmail.com>
2013-09-27 15:48:48 -0600

    added Playlist functions

--- FileManager/Playlist.h
+++ FileManager/Playlist.h
@@ -3,10 +3,19 @@
 
 using namespace std;
 
-struct Playlist
+class Playlist
 {
+  public:
   vector<Track*> tracks;
   int numTracks;
   char * filePath;
-  char * name;`
+  char * name;
+  
+  static PlayList * LoadPlayListFromFile(char *fname);
+  static bool SavePlayListToFile(PlayList *playList, char *fname);
+  bool SavePlayListToFile(char *fname);
+  bool AddTrack(Track *track);
+  bool RemoveTrack(Track *track);
+  bool RemoveTrack(char *name);
+  
 };
