f21ee2e5930f22fa970584959f09a077a288b1fa
jqt3of5 <jqt3of5@gmail.com>
2013-10-04 15:10:29 -0600

    added some code

--- FileManager/FileManager.h
+++ FileManager/FileManager.h
@@ -5,19 +5,36 @@
 class FileManager
 {
 
+  FileManager(char *musicDir, char * playlistDir);
+  
  public:
   //Intialize functions
-  FileManager(char *musicDir, char * playlistDir);
+  static FileManager GetFileManager(char * music = null, char * playlist = null)
+  {
+    if (_fileManagerInstance == null)
+    {
+     if (music != null && playlist != null) // should check if these dirs exist
+     {
+      _fileManagerInstance = new FileManager(music, playlist); 
+     }
+     
+    }
+    
+    return _fileManagerInstance;
+  }
+ 
   void RefreshMusicDB();
 
   //Playlist functions
   char ** ListPlayLists();
-  PlayList * LoadPlayList(char * name);
+  bool LoadPlayList(char * name, PlayList * playList);
   bool AddToPlayList(char * name, Track tracks[], int numTracks);
   bool RMPlaylist(char * name);
   
  private:
-  static PlayList * _activePlayList;
+ 
+  static FileManager * _fileManagerInstance = null;
+  PlayList * _activePlayList;
   char * musicRootDirectory;
   char * playListRootDirectory;
 
